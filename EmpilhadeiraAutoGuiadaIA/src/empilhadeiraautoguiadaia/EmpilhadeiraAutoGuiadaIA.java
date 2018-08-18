2b88d4d7eab6f0584443a84d3377e4a315fd31aa
Allan Lemes <aflemess@gmail.com>
2018-08-18 20:03:10 -0300

    Inicio do projeto

---
+++ EmpilhadeiraAutoGuiadaIA/src/empilhadeiraautoguiadaia/EmpilhadeiraAutoGuiadaIA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empilhadeiraautoguiadaia;
+
+/**
+ *
+ * @author skyli
+ */
+import src.view.Principal;
+public class EmpilhadeiraAutoGuiadaIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        principal.pack();
+        principal.setVisible(true);
+    }
+    
+}
